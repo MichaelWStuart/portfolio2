b3a20f1f3574b6352737f42f7baa93f26e56b28c
Michael Stuart <mike.w.stuart@gmail.com>
2017-05-09 18:05:22 -0700

    set up project skeleton

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(() => {
+
+  const projects = [
+    { title: 'Project 1', href: '#' },
+    { title: 'Project 2', href: '#' },
+    { title: 'Project 3', href: '#' }
+  //etc
+  ]
+
+  function Project(project) {
+    this.title = project.title;
+    this.href = project.href;
+  }
+
+});
