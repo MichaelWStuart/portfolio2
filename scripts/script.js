bf099a54b1960fb3484e824e912b6e09079c7fe8
Michael Stuart <mike.w.stuart@gmail.com>
2017-05-11 17:14:38 -0700

    implement handlebars.js, create project thumbnails, separate css according to SMACSS

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,22 @@
 
 $(() => {
 
-  const projects = [
-    { title: 'Vote App', thumb: '#', href: 'https://a-vote-app.herokuapp.com/' },
-    { title: 'Coordinator App', thumb: '#', href: 'https://coordinator-app.herokuapp.com/' },
-    { title: 'Exercise Tracker', thumb: '#', href: 'https://miwst.github.io/workout-tracker/' },
-    { title: 'Responsive JS Documentation', thumb: '#', href: 'https://nameless-river-85772.herokuapp.com/' },
-    { title: 'Roguelike Dungeon Crawler', thumb: '#', href: 'https://miwst.github.io/FCC-Dungeon-Crawler/' },
-    { title: 'Game of Life', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/yVjJxE/' },
-    { title: 'Drum Machine', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/RKOyzz/' },
-    { title: 'Random Dungeon Generator', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/aBQdpq/' },
-    { title: 'D3 Bar Chart', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/xgGmPq/' },
-    { title: 'D3 Heat Map', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/LxprYe/' },
-    { title: 'D3 Force Layout', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/ggrJOv/' },
-    { title: 'D3 Scatterplot', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/Xpmbyq/' },
-    { title: 'D3 Tree Map', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/mWeJeP/' },
-    { title: 'Recipe Box', thumb: '#', href: 'http://codepen.io/MichaelWStuart/full/NbbOQj/' },
-    { title: 'Simon Clone', thumb: '#', href: 'http://codepen.io/MichaelWStuart/pen/jVOrmY' },
-    { title: 'Javascript Calculator', thumb: '#', href: 'http://codepen.io/MichaelWStuart/pen/RGdBpq' }
-  ]
-
   function Project(project) {
-    this.title = project.title;
-    this.href = project.href;
-    this.thumb = project.thumb;
+    for (let key in project) {
+      this[key] = project[key];
+    }
   }
+
+  Project.prototype.toHtml = function () {
+    const template = $('#project-template').html();
+    const templateRender = Handlebars.compile(template);
+    return templateRender(this);
+  }
+
+  const projects = projectData.reduce((acc, project) => acc.concat(new Project(project)), []);
+
+  projects.forEach(project => $('#projects-div').append(project.toHtml()));
+
+  console.log($('#projects-div'))
 
 });
